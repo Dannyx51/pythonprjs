6db6041ea86afa0b55f16c5547a59cd7617ea3f1
dannyx51 <freinds528@gmail.com>
2020-05-17 11:17:53 -0400

    working

--- tryingSomeParticleStuff.py
+++ tryingSomeParticleStuff.py
@@ -1,2 +1,12 @@
 import pyglet
 from pyglet.window import mouse
+import numpy
+
+
+window = pyglet.window.Window(width = 1280, height = 720)
+
+class Particle:
+    
+    def __init__(self,x,y):
+        self.center = (x,y)
+        self.vertices = [(x+1,y), (), (), (), (), (), (), ()]
