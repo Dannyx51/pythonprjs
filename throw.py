64e6c4dd5613ca1a270f98e3facfd5cf878cc242
dannyx51 <freinds528@gmail.com>
2020-05-18 19:52:59 -0400

    oh yeah new thing

---
+++ throw.py
@@ -0,0 +1,11 @@
+import pyglet
+from pyglet.window import mouse
+import math 
+
+window = pyglet.window.Window(width = 1280, height = 720)
+
+def update(dt):
+    banana
+
+pyglet.clock.schedule_interval(update,1/60)
+pyglet.app.run()
