dc5ef9bbb8fd48fa674e2979b52579dc95f28e77
dannyx51 <freinds528@gmail.com>
2020-05-18 19:36:56 -0400

    re-arranging stuff because me dum dum

--- tryingSomeParticleStuff.py
+++ tryingSomeParticleStuff.py
@@ -2,12 +2,11 @@ import pyglet
 from pyglet.window import mouse
 import math
 
+window = pyglet.window.Window(width = 1280, height = 720)
 
 def calculateDistance(x1,y1,x2,y2):  
      dist = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)  
      return dist  
-
-window = pyglet.window.Window(width = 1280, height = 720)
 
 listfall = []
 listfell = []
