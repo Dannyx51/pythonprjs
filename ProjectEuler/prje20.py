16e1f001c008a05e5aed74b74d56bb0b7c954e42
dannyx51 <freinds528@gmail.com>
2020-12-30 03:47:47 -0500

    q20 donenanana

---
+++ ProjectEuler/prje20.py
@@ -0,0 +1,19 @@
+#hi yes hello starter comment
+
+
+x = 1
+
+for i in range(1,100):
+    x *= i
+
+s = str(x)
+
+#print(s)
+
+a = 0
+for i in range(len(s)):
+    a += int(s[i])
+
+print(a)
+
+#lololololololololol brute forced
