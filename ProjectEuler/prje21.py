0278021e9fe0946d757e1c22faa759e43fe873d0
dannyx51 <freinds528@gmail.com>
2021-01-01 19:07:14 -0500

    et fini

---
+++ ProjectEuler/prje21.py
@@ -0,0 +1,19 @@
+#sum of amicable numbers under 10000
+
+def d(num):
+    x = 0
+    for i in range(1,num+1):
+        if num % i == 0:
+            x += i
+    return x - num
+
+total = 0
+for i in range(2,10000):
+    if i == 1184:
+        print()
+    a = d(i)
+    b = d(a)
+    if i == b and a != b: 
+        total += i
+
+print(total)
