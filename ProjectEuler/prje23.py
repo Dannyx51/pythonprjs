d77d90c717c82c920eff1bc5b771c07c86086a85
Dannyx51 <freinds528@gmail.com>
2021-01-07 13:55:51 -0500

    Update prje23.py
    updated it with a working version W

--- ProjectEuler/prje23.py
+++ ProjectEuler/prje23.py
@@ -13,13 +13,32 @@ def d(num):
 #create list of [ab]undant numbers
 ab = []
 for i in range(1, 28124):
-    if d(i) > i:
-        ab.append(i)
+  print(i)
+  if d(i) > i:
+      ab.append(i)
 
 #create list of all possible creations
-pc = []
+#computing the numbers themselves takes too long so gonna try something else
+
+outloop = 0
+pc = [False for i in range(28124)]
+print(len(ab))
 for i in ab:
-    stop = False
-    pc.append(i)
-    #while(not stop):
-        
+  outloop += 1
+  print(outloop)
+  for x in ab:
+    if (i + x) > 28123:
+      break
+    pc[i + x] = True
+
+#finding the not possible numbers
+np = []
+for i in range(1,28124):
+  if pc[i] == False:
+    np.append(i)
+
+#summate
+total = 0
+for i in np:
+  total += i
+print(total)
