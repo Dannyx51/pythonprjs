141a56b49a96a6cb176264c68aa88f0c175bbda9
dannyx51 <freinds528@gmail.com>
2021-02-05 21:49:01 -0500

    help it brokey

--- EncryptionChallenge/primegen.py
+++ EncryptionChallenge/primegen.py
@@ -20,7 +20,45 @@ def baseTwoTest(a):
         else: return False
         if n % 2 != 0:  return True
                                                 
+def JSymbol(n):
+    D = 5
+    count = 0
+    while True:
+        v = D
+        v %= n
+        ret = 1
+        while v != 0:
+            while v % 2 == 0:
+                v //= 2
+                temp = n % 8
+                if temp in (3,5):
+                    ret *= -1
+            v, n = n, v
+            if v % 4 == 3 and n % 4 == 3:
+                ret *= -1
+            v %= n
+        
+        if n == 1 and ret == -1:
+            return D
+        else: 
+            if count % 2 == 0:
+                D = (D + 2) * - 1
+            else:
+                i = abs(D) + 2
+            count += 1
+            
 
 def newPrime(bit):
-    a = nextInt(bit)
+    while True:
+        a = nextInt(bit)
+        if not baseTwoTest(a): continue
+        D = JSymbol(a)
+        P = 1
+        Q = (1-D)//4
+
+        u1 = 1
+        v1 = 1
+
+
+
 
