78e7295b05ef2b1eb210651d752a219a8f4d9473
Dannyx51 <freinds528@gmail.com>
2021-03-16 12:43:49 -0400

    w

---
+++ ProjectEuler/prje55.py
@@ -0,0 +1,27 @@
+def isPal(n):
+    s = str(n)
+    ln = len(s)
+
+    fh = s[:ln//2]
+    bh = s[ln//2:] if ln % 2 == 0 else s[(ln//2) + 1:]
+    
+    return fh == bh[::-1]
+
+l = [True for i in range(10001)]
+
+for i in range(1,len(l)):
+    if not l[i]: continue
+
+    a,b = i, int(str(i)[::-1])
+
+    c = 0
+    while c < 50:
+        if isPal(a+b):
+            l[i] = False
+            break
+        a += b
+        b = int(str(a)[::-1])   
+        c += 1
+    
+fin = len([x for x in range(len(l)) if l[x]]) - 1
+print(fin) # -1 because 0 is true and i cba to fix it up above, this is easier
