bf77016530138c74e222450750b4663c90211b78
dannyx51 <freinds528@gmail.com>
2021-03-22 00:38:33 -0400

    Display in grid and sand simulation is perfecto

--- Random/testing some particles/falling and collision.py
+++ Random/testing some particles/falling and collision.py
@@ -4,37 +4,61 @@ import pygame.mixer
 from pygame.locals import *
 
 class particle:
-    def __init__(self, x:int, y:int, color:tuple) -> None:
+    def __init__(self, x:int, y:int, size:int, color:tuple) -> None:
         self.x,self.y = x,y
-        self.rect = pygame.Rect(self.x,self.y,6,6)
+        self.pSize = size
+        self.rect = pygame.Rect(self.x,self.y,self.pSize,self.pSize)
         self.color = color
         self.collide = False
-
 
     def display(self) -> None:
         pygame.draw.rect(screen,self.color,self.rect)
 
     def update(self, colTest: list) -> None:
-        if self.rect.move(0,1).bottom > height:
+        if self.rect.move(0,self.pSize).bottom > height:
             self.collide = True
-
+        
+        #if we have not already collided
         if not self.collide:
-            if self.rect.move(0,6).collidelist(colTest) != -1:
-                if self.rect.move(-6,6).collidelist(colTest) != -1:
-                    if self.rect.move(6,6).collidelist(colTest) != -1:
+            # can we go down?
+            if self.rect.move(0,self.pSize).collidelist(colTest) != -1:
+                # can we go left?
+                if self.rect.move(-self.pSize,self.pSize).collidelist(colTest) != -1:
+                    #can we go right?
+                    if self.rect.move(self.pSize,self.pSize).collidelist(colTest) != -1:
+                        #can't move any further, so i've 'collided', i will stop
                         self.collide = True
                     else:
-                        self.rect.move_ip(6,6)
+                        if self.rect.move(-self.pSize,self.pSize).right > width - 40:
+                            self.collide = True
+                        else:
+                            self.rect.move_ip(self.pSize,self.pSize)
                 else:
-                    self.rect.move_ip(-6,6)
+                    if self.rect.move(-self.pSize,self.pSize).left < 0:
+                        self.collide = True
+                    else:
+                        self.rect.move_ip(-self.pSize,self.pSize)
             else:
-                self.rect.move_ip(0,6)
+                self.rect.move_ip(0,self.pSize)
 
         self.display()
 
-pygame.init()
+def retClosest(x:int, l:list) -> int:
+    return l[(min((range(len(l))), key = lambda i: abs(l[i] - x)))]
 
 (width,height) = (400,400)
+
+divisions = 5
+gridx, gridy = [], []
+for i in range(0,width,divisions):
+    gridx.append(i)
+for i in range(0,height,divisions):
+    gridy.append(i)
+
+gX,gY = retClosest(width//2,gridx), retClosest(height//4,gridy)
+
+pygame.init()
+
 screen = pygame.display.set_mode((width,height))
 
 clock = pygame.time.Clock()
@@ -42,7 +66,7 @@ clock = pygame.time.Clock()
 pygame.display.set_caption("Particles")
 
 collided = []
-falling = [particle(width//2 - 10, height//4 - 10, blue)]
+falling = [particle(gX, gY, divisions, green)]
 
 counter = 0
 while True:
@@ -53,9 +77,15 @@ while True:
             pygame.quit()
     
     screen.fill(black)
-    
-    if counter % 6 == 0:
-        falling.append(particle(width//2 - 10, height//4 - 10, green))
+
+    keys = pygame.key.get_pressed()
+    if keys[K_LEFT]:
+        gX -= divisions if gX > 0 else 0
+    elif keys[K_RIGHT]:
+        gX += divisions if gX < width - divisions else 0
+
+    if counter % 3 == 0:
+        falling.append(particle(gX, gY, divisions, green))
 
     i = 0
     while i < len(falling):
