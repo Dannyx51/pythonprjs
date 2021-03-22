c46668819e2c7c9cb3f9ce2973a83488f9c5aaef
dannyx51 <freinds528@gmail.com>
2021-03-22 17:07:06 -0400

    some efficiency eh?

--- Random/testing some particles/falling and collision.py
+++ Random/testing some particles/falling and collision.py
@@ -4,61 +4,96 @@ import pygame
 import pygame.mixer
 from pygame.locals import *
 
+# look into this engine?
+# https://www.pygame.org/project/1387/2577
+
+#-------------------------------------------------#
+# Base particle class
 class particle:
     def __init__(self, x:int, y:int, size:int, color:tuple, speed: Optional[int] = 1) -> None:
-        self.pSize = size
+        self.size = size
         self.color = color
         self.speed = speed
-        self.rect = pygame.Rect(x,y,self.pSize,self.pSize)
+        self.rect = pygame.Rect(x,y,self.size,self.size)
         self.collide = False
 
     def display(self) -> None:
         pygame.draw.rect(screen,self.color,self.rect)
 
+# types of particle, diff attributes + update method
+# Base sand class
+class sand(particle):
+
     def update(self, colTest: list) -> None:
 
         # Where the particle will land if we continue down 
-        down = self.rect.move(0,(self.pSize * self.speed) if self.rect.bottom + (self.pSize * self.speed) < height else self.pSize) 
-        left = self.rect.move(-self.pSize,self.pSize)    # where partical will land if down-left
-        right = self.rect.move(self.pSize,self.pSize)    # where partical will land if down-right
+        down = self.rect.move(0,(self.size * self.speed) if self.rect.bottom + (self.size * self.speed) < height else self.size) 
+        left = self.rect.move(-self.size,self.size)    # where particle will land if down-left
+        right = self.rect.move(self.size,self.size)    # where particle will land if down-right
 
         if down.bottom > height:
             self.collide = True
 
         #if we have not already collided
         if not self.collide:
-            # can we go down?
-            if down.collidelist(colTest) != -1:
-                downNoSpeed = self.rect.move(0,self.pSize)
-                if downNoSpeed.collidelist(colTest) != -1:
-                    self.color = red
-                    # can we go left?
-                    if left.collidelist(colTest) != -1:
-                        #can we go right?
-                        if right.collidelist(colTest) != -1:
-                            #can't move any further, so i've 'collided', i will stop
-                            self.collide = True
-                        else:
-                            if right.right > width - 40:
-                                self.collide = True
-                            else:
-                                self.rect = right
-                    else:
-                        if left.left < 0:
-                            self.collide = True
-                        else:
-                            self.rect = left
-                else: self.rect = downNoSpeed
-            else:
+            if down.collidelist(colTest) == -1:
                 self.rect = down
+            elif left.collidelist(colTest) == -1:
+                self.color = red
+                self.rect = left
+            elif right.collidelist(colTest) == -1:
+                self.color = red
+                self.rect = right
+            else:
+                self.collide = True
 
         self.display()
+
+# Base water class
+class water(particle):
+    def __init__(self,*args,**kwargs) -> None:
+        super(water,self).__init__(*args,**kwargs)
+        self.blocked = False
+
+    def update(self, colTest: list) -> None:
+        global width
+        # Where the particle will land if we continue down 
+        down = self.rect.move(0,(self.size * self.speed) if self.rect.bottom + (self.size * self.speed) < height else self.size) 
+        
+        dleft = self.rect.move(-self.size,self.size)    # where partical will land if down-left
+        dright = self.rect.move(self.size,self.size)    # where partical will land if down-right
+        
+        left = self.rect.move(-self.size if self.rect.left - self.size > -1 else 0 ,0)
+        right = self.rect.move(self.size if self.rect.right + self.size < width - self.size else 0 ,0)
+
+        if down.bottom > height:
+            self.collide = True
+
+        #if we have not already collided
+        if not self.collide:
+            if down.collidelist(colTest) == -1:
+                self.rect = down
+            elif dleft.collidelist(colTest) == -1:
+                self.color = red
+                self.rect = left
+            elif dright.collidelist(colTest) == -1:
+                self.color = red
+                self.rect = right
+            else:
+                self.blocked = True
+
+        # Unique to liquids, can I spread to the sides? 
+         
+
+        self.display()
+
+#-------------------------------------------------#
 
 #returns the closest value to x that is in the list
 def retClosest(x:int, l:list) -> int:
     return l[(min((range(len(l))), key = lambda i: abs(l[i] - x)))]
 
-(width,height) = (1080,720) # self explanatory lol
+(width,height) = (550,400) # self explanatory lol
 
 # divisions is the pixel size of each pixel in the grid
 divisions = 5
@@ -85,7 +120,7 @@ clock = pygame.time.Clock()
 pygame.display.set_caption("Particles")
 
 collided = []
-falling = [particle(gX, gY, divisions, green, gSpeed)]
+falling = [sand(gX, gY, divisions, green, gSpeed)]
 
 counter = 0
 while True:
@@ -108,7 +143,7 @@ while True:
         gSpeed -= 1
 
     if counter % 3 == 0:
-        falling.append(particle(gX, gY, divisions, green, gSpeed))
+        falling.append(sand(gX, gY, divisions, green, gSpeed))
 
     i = 0
     while i < len(falling):
