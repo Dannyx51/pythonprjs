8543631308f2e6a2406650ea1f23b811cc5901ec
dannyx51 <freinds528@gmail.com>
2021-03-22 01:53:36 -0400

    speed ctrl & optimization

--- Random/testing some particles/falling and collision.py
+++ Random/testing some particles/falling and collision.py
@@ -1,61 +1,80 @@
 from colors import *
+from typing import Optional
 import pygame
 import pygame.mixer
 from pygame.locals import *
 
 class particle:
-    def __init__(self, x:int, y:int, size:int, color:tuple) -> None:
-        self.x,self.y = x,y
+    def __init__(self, x:int, y:int, size:int, color:tuple, speed: Optional[int] = 1) -> None:
         self.pSize = size
-        self.rect = pygame.Rect(self.x,self.y,self.pSize,self.pSize)
         self.color = color
+        self.speed = speed
+        self.rect = pygame.Rect(x,y,self.pSize,self.pSize)
         self.collide = False
 
     def display(self) -> None:
         pygame.draw.rect(screen,self.color,self.rect)
 
     def update(self, colTest: list) -> None:
-        if self.rect.move(0,self.pSize).bottom > height:
+
+        # Where the particle will land if we continue down 
+        down = self.rect.move(0,(self.pSize * self.speed) if self.rect.bottom + (self.pSize * self.speed) < height else self.pSize) 
+        left = self.rect.move(-self.pSize,self.pSize)    # where partical will land if down-left
+        right = self.rect.move(self.pSize,self.pSize)    # where partical will land if down-right
+
+        if down.bottom > height:
             self.collide = True
-        
+
         #if we have not already collided
         if not self.collide:
             # can we go down?
-            if self.rect.move(0,self.pSize).collidelist(colTest) != -1:
-                # can we go left?
-                if self.rect.move(-self.pSize,self.pSize).collidelist(colTest) != -1:
-                    #can we go right?
-                    if self.rect.move(self.pSize,self.pSize).collidelist(colTest) != -1:
-                        #can't move any further, so i've 'collided', i will stop
-                        self.collide = True
-                    else:
-                        if self.rect.move(-self.pSize,self.pSize).right > width - 40:
+            if down.collidelist(colTest) != -1:
+                downNoSpeed = self.rect.move(0,self.pSize)
+                if downNoSpeed.collidelist(colTest) != -1:
+                    self.color = red
+                    # can we go left?
+                    if left.collidelist(colTest) != -1:
+                        #can we go right?
+                        if right.collidelist(colTest) != -1:
+                            #can't move any further, so i've 'collided', i will stop
                             self.collide = True
                         else:
-                            self.rect.move_ip(self.pSize,self.pSize)
-                else:
-                    if self.rect.move(-self.pSize,self.pSize).left < 0:
-                        self.collide = True
+                            if right.right > width - 40:
+                                self.collide = True
+                            else:
+                                self.rect = right
                     else:
-                        self.rect.move_ip(-self.pSize,self.pSize)
+                        if left.left < 0:
+                            self.collide = True
+                        else:
+                            self.rect = left
+                else: self.rect = downNoSpeed
             else:
-                self.rect.move_ip(0,self.pSize)
+                self.rect = down
 
         self.display()
 
+#returns the closest value to x that is in the list
 def retClosest(x:int, l:list) -> int:
     return l[(min((range(len(l))), key = lambda i: abs(l[i] - x)))]
 
-(width,height) = (400,400)
+(width,height) = (1080,720) # self explanatory lol
 
+# divisions is the pixel size of each pixel in the grid
 divisions = 5
+
+# gridx and gridy hold the coordinate subdivisions for the display grid
+# for use in the retClosest functions
 gridx, gridy = [], []
 for i in range(0,width,divisions):
     gridx.append(i)
 for i in range(0,height,divisions):
     gridy.append(i)
 
-gX,gY = retClosest(width//2,gridx), retClosest(height//4,gridy)
+#global x and y coordinates
+gX,gY = retClosest(width//2,gridx), retClosest(0,gridy)
+#global particle speed
+gSpeed = 1
 
 pygame.init()
 
@@ -66,7 +85,7 @@ clock = pygame.time.Clock()
 pygame.display.set_caption("Particles")
 
 collided = []
-falling = [particle(gX, gY, divisions, green)]
+falling = [particle(gX, gY, divisions, green, gSpeed)]
 
 counter = 0
 while True:
@@ -83,9 +102,13 @@ while True:
         gX -= divisions if gX > 0 else 0
     elif keys[K_RIGHT]:
         gX += divisions if gX < width - divisions else 0
+    elif keys[K_UP]:
+        gSpeed += 1
+    elif keys[K_DOWN]:
+        gSpeed -= 1
 
     if counter % 3 == 0:
-        falling.append(particle(gX, gY, divisions, green))
+        falling.append(particle(gX, gY, divisions, green, gSpeed))
 
     i = 0
     while i < len(falling):
@@ -99,9 +122,9 @@ while True:
         else:
             i += 1
 
+    #display all of the pixels in final position
     for x in collided:
         x.display()
 
     pygame.display.flip()
     counter += 1
-
